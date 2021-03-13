fc94beca7fa3bc9b8822e35af9b1d10fa292b16c
Grzegorz Adamczyk <gadamczyk.net@gmail.com>
2021-03-13 16:02:29 +0100

    1. Building a Notification
    Create class "Notification". A Notification collects together errors

---
+++ RefactoringExceptions.Core/Notification.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RefactoringExceptions.Core
+{
+    public class Notification
+    {
+        private List<string> errors = new List<string>();
+        public void AddError(string message) => errors.Add(message);
+        public bool HasErrors() => errors.Count != 0;
+    }
+}
